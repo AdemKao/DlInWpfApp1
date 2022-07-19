987d632924b9a2af5d488014c279afc0bdebe83d
AdemKao <adem60513@gmail.com>
2022-07-19 09:26:50 +0800

    add App.xmal.cs

--- WpfApp1/App.xaml.cs
+++ WpfApp1/App.xaml.cs
@@ -1,11 +1,38 @@
-﻿using System.Windows;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Windows;
 
-namespace WpfApp1
+namespace WpfApp1;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    public static IHost? AppHost { get; private set; }
+
+    public App()
     {
+        AppHost = Host.CreateDefaultBuilder()
+            .ConfigureServices((hostContext, services) =>
+            {
+
+            })
+            .Build();
+    }
+
+    protected override async void OnStartup(StartupEventArgs e)
+    {
+        await AppHost!.StartAsync();
+
+        var startupForm = AppHost.Services.GetRequiredService<MainWindow>();
+        startupForm.Show();
+
+        base.OnStartup(e);
+    }
+    protected override async void OnExit(ExitEventArgs e)
+    {
+        await AppHost!.StopAsync();
+        base.OnExit(e);
     }
 }
